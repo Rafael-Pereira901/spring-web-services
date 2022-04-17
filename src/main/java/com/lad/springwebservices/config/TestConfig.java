d296b514fee427940540b062ae904fee795349a5
Rafael Pereira <rafaelpereira901@gmail.com>
2022-04-17 18:53:15 -0300

    Payment one to one association

--- src/main/java/com/lad/springwebservices/config/TestConfig.java
+++ src/main/java/com/lad/springwebservices/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lad.springwebservices.entities.Category;
 import com.lad.springwebservices.entities.Order;
 import com.lad.springwebservices.entities.OrderItem;
+import com.lad.springwebservices.entities.Payment;
 import com.lad.springwebservices.entities.Product;
 import com.lad.springwebservices.entities.User;
 import com.lad.springwebservices.enums.OrderStatus;
@@ -81,7 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
 		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	
 	}
 
